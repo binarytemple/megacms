bf00a06cafca3ce14e065012ecc84d196e8778e2
Oliver Brook <olibrook@gmail.com>
2013-03-06 00:30:01 -0800

    Added failing test for creating new widgets

---
+++ src/main/tests/megacms/frontend/testviews.py
@@ -0,0 +1,8 @@
+from tests.base import DatastoreTestCase
+
+
+class CreateElementViewTest(DatastoreTestCase):
+
+    def test_1(self):
+        """Should allow a user to create a new widget as the child of an existing one"""
+        self.fail()
